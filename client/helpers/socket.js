4263bd0c1ebda20f2ebbfdfd919a7d292e1101f4
Rajavel Balasubramanian <bsubramanian.a@gmail.com>
2023-07-04 01:30:10 +0530

    direct socket change

--- client/helpers/socket.js
+++ client/helpers/socket.js
@@ -1,9 +1,10 @@
 import { io } from 'socket.io-client';
 
-const isDev = process.env.NODE_ENV === 'development';
+//const isDev = process.env.NODE_ENV === 'development';
 
 
 //const socket = io(isDev ? 'ws://whatsapp-clone-woad-two.vercel.app' : 'ws://whatsapp-clone-woad-two.vercel.app');
 //const socket = io(isDev ? 'ws://localhost:8080' : '/');
 //const socket = io(isDev ? 'ws://imaxchat-7n7x.onrender.com' : '/');
+const socket = io('ws://whatsapp-clone-woad-two.vercel.app');
 export default socket;
