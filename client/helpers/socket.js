c8a4668bb95d168aa61c00285c204ced7f6939a9
Rajavel Balasubramanian <bsubramanian.a@gmail.com>
2023-07-04 01:18:33 +0530

    url change

--- client/helpers/socket.js
+++ client/helpers/socket.js
@@ -3,7 +3,7 @@ import { io } from 'socket.io-client';
 const isDev = process.env.NODE_ENV === 'development';
 
 
-const socket = io(isDev ? 'ws://whatsapp-clone-service.onrender.com' : '/');
+const socket = io(isDev ? 'ws://whatsapp-clone-woad-two.vercel.app' : 'ws://whatsapp-clone-woad-two.vercel.app');
 //const socket = io(isDev ? 'ws://localhost:8080' : '/');
 //const socket = io(isDev ? 'ws://imaxchat-7n7x.onrender.com' : '/');
 export default socket;
