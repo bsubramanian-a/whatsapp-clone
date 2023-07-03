e0c4fe91a0776df598ad0c2e9c8dc9e217cbfe4e
Rajavel Balasubramanian <bsubramanian.a@gmail.com>
2023-07-04 00:00:25 +0530

    8080

--- client/helpers/socket.js
+++ client/helpers/socket.js
@@ -3,7 +3,7 @@ import { io } from 'socket.io-client';
 const isDev = process.env.NODE_ENV === 'development';
 
 
-const socket = io(isDev ? 'ws://whatsapp-clone-service.onrender.com:8080' : '/');
+const socket = io(isDev ? 'ws://whatsapp-clone-service.onrender.com' : '/');
 //const socket = io(isDev ? 'ws://localhost:8080' : '/');
 //const socket = io(isDev ? 'ws://imaxchat-7n7x.onrender.com' : '/');
 export default socket;
